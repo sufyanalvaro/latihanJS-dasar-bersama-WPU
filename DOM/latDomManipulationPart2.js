b2596ac2cac6261533766d6bafea72ba5bb0b99e
sufyanalvaro <sufyan35@gmail.com>
2024-04-19 10:43:49 +0700

    DOM

---
+++ DOM/latDomManipulationPart2.js
@@ -0,0 +1,4 @@
+// buat elemen baru
+
+const pBaru = ducument.createElement("p");
+const teksPBaru = document.createAttributeTextNode("Paragraf Baru");
---
+++ JS LANJUTAN/thisArrowFunction.js
@@ -0,0 +1,39 @@
+// construktor function
+// const Mahasiswa = function () {
+//   this.nama = "Sandhika";
+//   this.umur = 33;
+//   this.sayHello = function () {
+//     console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur}`);
+//   };
+// };
+
+// const sandhika = new Mahasiswa();
+
+// Arrow function
+
+// const Mahasiswa = function () {
+//   this.nama = "Sandhika";
+//   this.umur = 33;
+//   this.sayHello = () => {
+//     console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur}`);
+//   };
+// };
+
+// const sandhika = new Mahasiswa();
+// note : arrow function tidak mengenla konsep this
+
+// literal . catet dlu deh,, nnti liat vids lagi kalo bingung
+
+// const Mahasiswa = function () {
+//   this.nama = "Sandhika";
+//   this.umur = 33;
+//   this.sayHello = () => {
+//     console.log(`Halo, nama saya ${this.nama}, dan saya ${this.umur}`);
+//   };
+
+//   setInterval(() => {
+//     console.log(this.umur++);
+//   }, 2000);
+// };
+
+// const sandhika = new Mahasiswa();
